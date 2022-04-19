a6a0c2fd5bcae48c21614e729e373430aa4f5075
jtaulman <jtaulman23@gmail.com>
2022-04-18 23:50:41 -0400

    Challenge Deliverable 3

---
+++ MechaCar_Challenge/MechaCar_Challenge_3.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(tidyverse)
+mechacar_mpg <- file.choose('/MechaCar_Challenge/MechaCar_mpg.csv')
+mechacar <- read.csv(mechacar_mpg,check.names = F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar))
+
+Mecha_Suspension_coil <- file.choose('/Suspension_Coil.csv')
+mecha_coil <- read.csv(Mecha_Suspension_coil,check.names = F,stringsAsFactors = F)
+total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')
+lot_summary <- mecha_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')
+
+t.test(mecha_coil$PSI,mu=1500)
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
